dee2c6335324ca35a4649d7c77f574276b53cc75
Jason Orendorff <jason.orendorff@gmail.com>
2017-02-26 09:49:14 -0600

    Code to read the test files in `tests/select`.

--- jorendorff+elixir/test/tiny_rdbms_test.exs
+++ jorendorff+elixir/test/tiny_rdbms_test.exs
@@ -10,4 +10,19 @@ end
 defmodule SqlTest do
   use ExUnit.Case
   doctest Sql
+
+  test "select examples" do
+    dir = Path.expand(Path.join(__DIR__, "../../tests/select"))
+    for f <- File.ls!(dir) do
+      if String.match?(f, ~r/^[0-9][0-9]-.*\.md$/) do
+        IO.puts(f)
+        text = File.read!(Path.join(dir, f))
+        [query, answer] = Regex.run(
+          ~r/^.*\n\n## Query\n\n((?:    .*\n)+)\n## Answer\n\n((?:    .*\n)*)$/,
+          text,
+          capture: :all_but_first)
+
+      end
+    end
+  end
 end
