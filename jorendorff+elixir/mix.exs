c0c3cbf9a82eeb04a8f1aa642426e3cdf1e3f428
Jason Orendorff <jason.orendorff@gmail.com>
2017-02-27 11:58:07 -0600

    Run the SELECT tests.

--- jorendorff+elixir/mix.exs
+++ jorendorff+elixir/mix.exs
@@ -28,6 +28,8 @@ defmodule TinyRdbms.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      {:csv, "~> 1.4.2"}
+    ]
   end
 end
--- jorendorff+elixir/test/tiny_rdbms_test.exs
+++ jorendorff+elixir/test/tiny_rdbms_test.exs
@@ -2,27 +2,35 @@ defmodule TinyRdbmsTest do
   use ExUnit.Case
   doctest TinyRdbms
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "select examples" do
+    tables_dir = Path.expand(Path.join(__DIR__, "../../tests/tables"))
+    database = TinyRdbms.load(tables_dir)
+
+    dir = Path.expand(Path.join(__DIR__, "../../tests/select"))
+    for f <- File.ls!(dir) do
+      if String.match?(f, ~r/^[0-9][0-9]-.*\.md$/) do
+        IO.puts(f)
+        text = File.read!(Path.join(dir, f))
+        [query, answer_str] = Regex.run(
+          ~r/^.*\n\n## Query\n\n((?:    .*\n)+)\n## Answer\n\n((?:    .*\n)*)$/,
+          text,
+          capture: :all_but_first)
+
+        expected_answer =
+          Regex.scan(~r/    (.*\n)/, answer_str, capture: :all_but_first) |>
+          Enum.map(fn([line]) -> line end) |>
+          CSV.decode() |>
+          Enum.to_list()
+
+        actual_answer = TinyRdbms.run_query(database, query)
+
+        assert actual_answer == expected_answer
+      end
+    end
   end
 end
 
 defmodule SqlTest do
   use ExUnit.Case
   doctest Sql
-
-  test "select examples" do
-    dir = Path.expand(Path.join(__DIR__, "../../tests/select"))
-    for f <- File.ls!(dir) do
-      if String.match?(f, ~r/^[0-9][0-9]-.*\.md$/) do
-        IO.puts(f)
-        text = File.read!(Path.join(dir, f))
-        [query, answer] = Regex.run(
-          ~r/^.*\n\n## Query\n\n((?:    .*\n)+)\n## Answer\n\n((?:    .*\n)*)$/,
-          text,
-          capture: :all_but_first)
-
-      end
-    end
-  end
 end
