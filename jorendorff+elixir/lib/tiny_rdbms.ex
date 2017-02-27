fe1ea691a6f47c10ce67d46861db31907cae44b3
Jason Orendorff <jason.orendorff@gmail.com>
2017-02-27 12:02:45 -0600

    Parse and run basic SQL queries.

--- jorendorff+elixir/lib/tiny_rdbms.ex
+++ jorendorff+elixir/lib/tiny_rdbms.ex
@@ -3,19 +3,67 @@ defmodule TinyRdbms do
   Documentation for TinyRdbms.
   """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> TinyRdbms.hello
-      :world
-
-  """
-  def hello do
-    :world
+  def load_table(filename) do
+    File.stream!(filename) |>
+      CSV.decode(headers: true) |>
+      Enum.to_list()
   end
 
+  def load(dir) do
+    File.ls!(dir) |>
+      Enum.filter(fn(f) -> String.ends_with?(f, ".csv") end) |>
+      Enum.map(fn(f) ->
+        {String.downcase(String.slice(f, 0..-5)),
+         TinyRdbms.load_table(Path.join(dir, f))}
+      end) |>
+      Enum.reduce(%{}, fn({k, v}, db) -> Map.put(db, k, v) end)
+  end
+
+  def run_query(database, query) do
+    ast = Sql.parse_select_stmt!(query)
+    ast |> inspect() |> IO.puts()
+    [table_name] = ast.from
+
+    results = Map.get(database, String.downcase(table_name))
+
+    # Evalute WHERE clause.
+    where_expr = ast.where
+    results = Enum.filter(results, fn(row) ->
+      is_sql_value_truthy?(eval_expr(row, where_expr))
+    end)
+
+    # Execute SELECT clause.
+    select_exprs = ast.select
+    results = Enum.map(results, fn(row) ->
+      # For each row, evaluate each selected expression.
+      Enum.map(select_exprs, fn(expr) -> eval_expr(row, expr) end)
+    end)
+
+    results
+  end
+
+  defp eval_expr(row, expr) do
+    case expr do
+      {:identifier, x} -> row[x]
+      {:number, n} -> n
+      {:=, left_expr, right_expr} ->
+        left_val = eval_expr(row, left_expr)
+        right_val = eval_expr(row, right_expr)
+        sql_equals?(left_val, right_val)
+      _ -> raise ArgumentError, message: "internal error: unrecognized expr #{inspect(expr)}"
+    end
+  end
+
+  defp is_sql_value_truthy?(v) do
+    v == true || (is_integer(v) && v != 0)
+  end
+
+  defp sql_equals?(left, right) do
+    left == right ||
+      ((is_binary(left) || is_integer(left)) &&
+       (is_binary(right) || is_integer(right)) &&
+       to_string(left) == to_string(right))
+  end
 end
 
 
@@ -92,6 +140,96 @@ defmodule Sql do
             {:string, token_str} # TODO: parse string
           true -> {:error, "unrecognized character: #{token_str}"}
         end
+    end
+  end
+
+  def parse_select_stmt!(sql) do
+    {%{}, tokenize(sql)} |>
+      parse_clause!(:select, &parse_exprs!/1, required: true) |>
+      parse_clause!(:from, &parse_tables!/1) |>
+      parse_clause!(:where, &parse_expr!/1) |>
+      parse_clause_2!(:group, :by, &parse_exprs!/1) |>
+      parse_clause!(:having, &parse_expr!/1) |>
+      parse_clause_2!(:order, :by, &parse_exprs!/1) |>
+      check_done!()
+  end
+
+  defp parse_exprs!(sql) do
+    {expr, tail} = parse_expr!(sql)
+    case tail do
+      [:',' | more] ->
+        {exprs, rest} = parse_exprs!(more)
+        {[expr | exprs], rest}
+      _ -> {[expr], tail}
+    end
+  end
+
+  defp parse_prim!(sql) do
+    case sql do
+      [{:identifier, _} | tail] -> {hd(sql), tail}
+      [{:number, _} | tail] -> {hd(sql), tail}
+      _ -> raise ArgumentError, message: "columns expected"
+    end
+  end
+
+  defp parse_expr!(sql) do
+    {prim, rest} = parse_prim!(sql)
+    case rest do
+      [:= | rest] ->
+        {rhs, rest} = parse_prim!(rest)
+        {{:=, prim, rhs}, rest}
+      _ -> {prim, rest}
+    end
+  end
+
+  defp parse_table!(sql) do
+    case sql do
+      [{:identifier, x} | rest] -> {x, rest}
+      _ -> raise ArgumentError, message: "table name expected"
+    end
+  end
+
+  defp parse_tables!(sql) do
+    {table, rest} = parse_table!(sql)
+    case rest do
+      [:',' | rest] ->
+        {tables, rest} = parse_tables!(rest)
+        {[table | tables], rest}
+      _ -> {[table], rest}
+    end
+  end
+
+  defp parse_clause!({ast, sql}, keyword, parser, keywords \\ []) do
+    case sql do
+      [^keyword | tail] ->
+        {clause_ast, rest} = parser.(tail)
+        {Map.put(ast, keyword, clause_ast), rest}
+      _ ->
+        if Keyword.get(keywords, :required, false) do
+          raise ArgumentError, message: "#{keyword} expected"
+        else
+          {Map.put(ast, keyword, :nil), sql}
+        end
+    end
+  end
+
+  defp parse_clause_2!({ast, sql}, kw1, kw2, parser) do
+    case sql do
+      [^kw1, ^kw2 | tail] ->
+        {clause_ast, rest} = parser.(tail)
+        {Map.put(ast, kw1, clause_ast), rest}
+      _ ->
+        {Map.put(ast, kw1, :nil), sql}
+    end
+  end
+
+  defp check_done!({ast, sql}) do
+    case sql do
+      [] -> ast
+      _ ->
+        ast |> inspect() |> IO.puts()
+        sql |> inspect() |> IO.puts()
+        raise ArgumentError, message: "extra stuff at end of SQL"
     end
   end
 end
