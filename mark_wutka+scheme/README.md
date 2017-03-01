5f6a421783a84ad0147b956949239c8f389d6306
Mark Wutka <mark.wutka@banyanhills.com>
2017-03-01 13:37:04 -0600

    Added Scheme implementation

---
+++ mark_wutka+scheme/README.md
@@ -0,0 +1,65 @@
+# Scheme RDBMS Query
+
+This is an implementation in Chicken Scheme of a query engine
+for the Chinook database. It can join efficiently across multiple
+tables and has support for order-by.
+
+The trick that this engine uses for making the queries efficient
+is something of a "poison pill", although in this case it is more
+of a happy pill. For example, consider the following query (in SQL,
+not the s-expr used by the engine):
+```sql
+SELECT album.Title, track.Name FROM album, artist, track 
+    WHERE artist.Name="AC/DC" AND artist.ArtistId=album.ArtistId AND
+          track.AlbumId=album.AlbumID
+```
+
+If we try to create a cartesian product of all three tables, we would
+have more than 300 million rows to process. But, what if we first filter
+the artist table for artist.Name="AC/DC" and then do a product with
+album, filtering that down to the AC/DC albums, then doing a product
+with track.
+
+But, how do we filter a subset of the tables using the WHERE clause?
+
+When we first filter the album table, what if we replace any column
+comparisons with true if the table hasn't been processed yet? That is,
+when we filter album, the WHERE clause effectively becomes:
+WHERE artist.Name="AC/DC" AND true AND true
+
+Then, once we join with artist, the WHERE clause becomes:
+WHER artist.Name="AC/DC" AND artist.ArtistId=album.ArtistId AND true
+
+And finally after bring in track, the full WHERE clause is evaluated
+against the remaining rows.
+
+This engine represents the WHERE clause as an s-expr, so the above WHERE
+clause is represented as:
+```scheme
+(db-and (equal? '("artist" . "Name") "AC/DC") 
+        (equal? '("artist" . "ArtistId") '("album" . "ArtistId"))
+        (equal? '("track" . "AlbumId") '("album" . "AlbumId")))
+```
+
+When this expression is evaluated, any column references are replaced
+either with the value of the column, or a special "unavailable" symbol.
+Whenever a function is to be executed, except for db-and and db-or, the
+arguments are examined and if any are unavailable, the function result
+is automatically unavailable, so (equal? unavailable "something") has
+a result of "unavailable".
+
+This is dependent on the ordering of the tables being joined, so that
+if track and album are joined first, we have to filter a fill cartesian
+product of these two tables because there is no way to create a subset.
+But, if artist comes first, we reduce that table to one row - the one
+containing "AC/DC".
+
+An optimization that could be added would be to look for comparisons
+between a column and constant, and move the tables with those columns
+to the beginning of the list.
+
+## To Do
+
+It would be nice to create a SQL query parser that translates a SELECT
+statement into the s-expr for the engine. Perhaps the parser could have
+the intelligence to optimize the table order as well.
---
+++ mark_wutka+scheme/database.scm
@@ -0,0 +1,155 @@
+(require-extension lookup-table)
+(require-extension srfi-13)
+(require-extension data-structures)
+(require-extension extras)
+
+;; This is the basic databas engine
+;;
+;; Define a tag used to "poison" part of a where clause to
+;; consider that part as being true
+(define unavailable (gensym 'unav))
+
+;; Load the table, splitting the CSV values and creating a list of dictionaries
+;; where each row has a column->value dictionary
+(define (load-table table-name)
+  (with-input-from-file (string-concatenate (list "../tests/tables/" table-name ".csv"))
+    (lambda()
+      (let* ((lines (map (lambda (s) (string-split s ",")) (read-lines)))
+             (columns (car lines))
+             (data (cdr lines)))
+        (map (lambda (d) (alist->dict (map cons columns d) equal?)) data)))))
+
+;; This is a specialized cartesian product that assumes that the first item can be a
+;; multi-valued list, but the second only a single-valued list. It is used to progressively
+;; join table columns, where the first two tables are joined, the results filtered, and
+;; then the next table is joined to the previous resuls. A row is a list of (table . dict) values
+;; where table is the table that the values came from, and dict is the dictionary of values
+(define (product-1 a b all-b)
+  (if (null? b) (product-1 (cdr a) all-b all-b)
+    (if (null? a) '()
+      (cons (cons (caar b) (car a)) (product-1 a (cdr b) all-b)))))
+(define (product a b) (product-1 a b b))
+
+;; Retrieve the dictionary for a named table from the row, or unavailable if the
+;; row doesn't have values from that table yet
+(define (table-by-name t row)
+  (let ((tab (assoc t row)))
+    (if tab (cdr tab)
+      unavailable)))
+
+;; Retrieve a value from a table, or unavailable if either the value isn't in the table
+;; (although that shouldn't happen) or if the table is still unavailable
+(define (value-by-name v tab)
+  (if (equal? tab unavailable) unavailable
+    (dict-ref tab v unavailable)))
+
+;; Normally, if any of the args to a function are unavailable, the function result
+;; is also unavailable. But, in order to properly filter, we need the db-and and db-or
+;; functions to go ahead and evaluate. Since unavailable isn't #f, it is treated as true.
+(define (apply-func func args)
+  (if (or (eq? func db-and) (eq? func db-or))
+    (apply func args)
+    (if (member unavailable args) unavailable
+      (apply func args))))
+
+;; Evaluates a where expression. If it sees a pair with (table-name . column-name) it
+;; retrieves the value from table-assoc, which is the current row. Otherwise, if it
+;; sees a list that starts with a procedure, it evaluates all the arguments and then
+;; calls the procedure. If any argument comes back as unavailable, the result of the
+;; expression is unavailable.
+(define (eval-expr expr table-assoc)
+  (if (pair? expr)
+    (if (procedure? (car expr))
+      (let ((args (map (lambda (v) (eval-expr v table-assoc)) (cdr expr)))
+            (func (car expr)))
+        (apply-func func args))
+      (let* ((t (car expr))
+            (col (cdr expr))
+            (tab (assoc t table-assoc)))
+        (if tab
+          (dict-ref (cdr tab) col unavailable)
+          unavailable)))
+    expr))
+
+;; Load the tables
+(define album (load-table "album"))
+(define artist (load-table "artist"))
+(define customer (load-table "customer"))
+(define employee (load-table "employee"))
+(define genre (load-table "genre"))
+(define invoice (load-table "invoice"))
+(define invoiceline (load-table "invoiceline"))
+(define mediatype (load-table "mediatype"))
+(define playlist (load-table "playlist"))
+(define playlisttrack (load-table "playlisttrack"))
+(define track (load-table "track"))
+
+;; Create a table dictionary
+(define table-list '(("album" album) ("artist" artist) ("customer" customer)
+                     ("employee" employee) ("genre" genre) ("invoice" invoice)
+                     ("invoiceline" invoiceline) ("mediatype" mediatype)
+                     ("playlist" playlist) ("playlisttrack" playlisttrack)
+                     ("track" track)))
+
+;; Given a list of table names, return a list of (table-name table) pairs
+(define (from tables)
+  (map (lambda (t) (assoc t table-list)) tables))
+
+;; Given a list of (table-name . column-name) values, retrieve the values from the row
+(define (get-values vs row)
+  (if (null? vs) '()
+    (cons (value-by-name (cdar vs) (table-by-name (caar vs) row))
+          (get-values (cdr vs) row))))
+
+;; For a named table, load the data from the table, making each table row into
+;; a pair (table-name . row)
+(define (get-table-rows table-pair)
+  (map (lambda (r) (list (cons (car table-pair) r))) (eval (cadr table-pair))))
+
+;; Remove any rows that don't match the where clause
+(define (filter-result where tables)
+  (filter (lambda (r) (eval-expr where r)) tables))
+
+;; Given the current result and another table to add in, filter the new table
+;; then do a cartesian product and then filter the results
+(define (join-rows t1 t2 where)
+  (filter-result where
+    (product t1 (filter-result where (get-table-rows t2)))))
+
+;; Returns all the rows that match the where clause for the list of tables
+(define (get-rows tables where)
+  (fold (lambda (t2 t1) (join-rows t1 t2 where))
+        (filter-result where (get-table-rows (car tables))) (cdr tables)))
+
+;; Compares two rows for sorting
+(define (row-compare r1 r2 order-list)
+  (if (null? order-list) #t
+    (let* ((curr-col (car order-list))
+           (r1-value (value-by-name (cdr curr-col) (table-by-name (car curr-col) r1)))
+           (r2-value (value-by-name (cdr curr-col) (table-by-name (car curr-col) r2))))
+      (if (string= r1-value r2-value) (row-compare r1 r2 (cdr order-list))
+        (string< r1-value r2-value)))))
+
+;; Performs a query and returns the values
+(define (do-query vals tables where order-by)
+  (map (lambda (r) (get-values vals r))
+       (sort (get-rows (from tables) where)
+             (lambda (r1 r2) (row-compare r1 r2 order-by)))))
+
+;; Helper functions for queries
+(define (db-and . l) (every (lambda (x) x) l))
+(define (db-or . l) (any (lambda (x) x) l))
+
+;; Here is an example query using this engine:
+;; (do-query '(("album" . "Title")) '("artist" "album") (list db-and (list equal? '("artist" . "Name") "Led Zeppelin") (list equal? '("artist" . "ArtistId") '("album" . "ArtistId"))) '(("album" . "Title")))
+;;
+;; This query selects album.Title from the artist and album tables, looking for
+;; artist.Name = "Led Zeppelin" and artist.ArtistId = album.ArtistId
+;; then does an order-by album.Title
+;; 
+;; Here's a query that spans three tables and still returns very quickly:
+;; (do-query '(("album" . "Title") ("track" . "Name")) '("artist" "album" "track") (list db-and (list equal? '("artist" . "Name") "AC/DC") (list equal? '("artist" . "ArtistId") '("album" . "ArtistId")) (list equal? '("track" . "AlbumId") '("album" . "AlbumId"))) '(("album" . "Title") ("track" . "Name")))
+;;
+;; The next task is to build a simple parser to take something
+;; like a standard SQL command and generate the do-query expression that
+;; would execute the query
